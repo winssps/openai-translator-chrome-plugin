d9645f4a11d12996a946ab8c3c4d2f8c263be8ad
winssps <mingwei@caih.com>
2023-03-09 21:32:01 +0800

    修改表单

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,54 @@
 import { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import browser from "webextension-polyfill";
+import Avatar from "@mui/material/Avatar";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 
 function LoginForm() {
-  const [password, setPassword] = useState("");
-
   const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log("Submitted!", password);
+    const data = new FormData(event.currentTarget);
+    console.log(data.get("APIKey"));
 
-
-    console.log(browser.storage.sync.set)
-
-
+    try {
+      console.log(browser)
+      const apiKey = data.get("APIKey");
+      await browser.storage.sync.set({ apiKey });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <TextField
-        required
-        label="API Key"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <Button variant="contained" type="submit">
-        保存
-      </Button>
-    </form>
+    <Container component="main" maxWidth="xs">
+      <Typography component="h1" variant="h5">
+        设置API Key
+      </Typography>
+      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="APIKey"
+          label="API Key"
+          type="password"
+          id="APIKey"
+          autoComplete="current-password"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          保存
+        </Button>
+      </Box>
+    </Container>
   );
 }
 
